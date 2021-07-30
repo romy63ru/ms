2eeab9728e4fe6a3a89a8bac21d523a8ff7aa2f2
Aleksandr Romanov <AleksandrR@oriflame.com>
2021-07-30 16:40:06 +0200

    1.6

--- src/ArraysAndStrings.cs
+++ src/ArraysAndStrings.cs
@@ -8,7 +8,7 @@ namespace src
 {
     public class ArraysAndStrings
     {
-        // Реализуйте алгоритм, определяющий, все ли символы в строке встречаются
+        // 1.1 Реализуйте алгоритм, определяющий, все ли символы в строке встречаются
         // только один раз. А если при этом запрещено использование дополнительных
         // структур данных?
         public static bool CheckString(string input)
@@ -29,6 +29,41 @@ namespace src
                 }
             }
             return true;
+        }
+
+        //1.6 Реализуйте метод для выполнения простейшего сжатия строк с использованием счетчика повторяющихся символов. Например, строка ааЬсссссааа превращается в а2Ыс5аЗ. Если ~сжатая» строка не становится короче исходной,
+        //то метод возвращает исходную строку.Предполагается, что строка состоит
+        //только из букв верхнего и нижнего регистра(a-z).
+
+        //todo разобрать решение и сделать проще
+
+        public static string CompressString(string input)
+        {
+            string output = input[0].ToString();
+            int count = 1;
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (output[output.Length-1] == input[i])
+                {
+                    count++;
+                } else
+                {
+                    output += count.ToString();
+                    output += input[i];
+                    count = 1;
+                }
+            }
+
+            output += count.ToString();
+
+            if (output.Length >= input.Length)
+            {
+                return input;
+            }
+            else
+            {
+                return output;
+            }
         }
     }
 }
--- test/ArrayAndStringsTest.cs
+++ test/ArrayAndStringsTest.cs
@@ -1,4 +1,4 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace src
 {
@@ -8,12 +8,17 @@ namespace src
         [TestMethod]
         public void CheckStringTest1a1()
         {
-            var t = new ArraysAndStrings();
             Assert.IsFalse(ArraysAndStrings.CheckString("abbbbbcccc"));
             Assert.IsFalse(ArraysAndStrings.CheckString("abca"));
             Assert.IsTrue(ArraysAndStrings.CheckString("abcd"));
             Assert.IsTrue(ArraysAndStrings.CheckString("a"));
             Assert.IsTrue(ArraysAndStrings.CheckString(""));
+        }
+
+        [TestMethod]
+        public void CompressStringTest1a6()
+        {
+            Assert.AreEqual(ArraysAndStrings.CompressString("aabcccccaaa"), "a2b1c5a3");
         }
     }
 }
