b4e0227d3ba9e8844c301caa563ed1f88c3deb24
Alex Romanov <romy63ru@hotmail.com>
2021-08-01 21:14:56 +0200

    * Lists.cs: * ListsTest.cs:
    * src.csproj.nuget.dgspec.json: 2.1. Write code to remove duplicates
      from an unsorted linked list.

---
+++ src/Lists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace src
+{
+    public class Lists
+    {
+
+        //2.1 Remove Dups! Write code to remove duplicates from an unsorted linked list.
+        public LinkedList<int> RemoveDuplicates2and1(LinkedList<int>  input)
+        {
+            var result = new LinkedList<int>();
+            var n = input.First;
+            result.AddLast(n.Value);
+            input.RemoveFirst();
+            while (input.First.Next != null)
+            {
+                n = input.First;
+                if (!result.Contains(n.Value))
+                {
+                    result.AddLast(n.Value);
+                }
+                input.RemoveFirst();
+            }
+            return result;
+        }
+    }
+}
---
+++ test/ListsTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace src
+{
+    [TestClass]
+    public class ListsTest
+    {
+        [TestMethod]
+        public void RemoveDuplicates2and1Test()
+        {
+            var lists = new Lists();
+            var input = new LinkedList<int>();
+            input.AddLast(2);
+            input.AddLast(4);
+            input.AddLast(1);
+            input.AddLast(0);
+            input.AddLast(7);
+            input.AddLast(4);
+            input.AddLast(2);
+            input.AddLast(1);
+
+            var result = lists.RemoveDuplicates2and1(input);
+            Assert.AreEqual(result.Count, 5);
+            Assert.AreEqual(result.First.Value, 2);
+            Assert.AreEqual(result.Last.Value, 7);
+        }
+    }
+}
