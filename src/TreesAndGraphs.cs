352c84ae147bcafa698a3ec43a8da31d745b8328
Alex Romanov <romy63ru@hotmail.com>
2021-08-01 16:23:35 +0200

    * TreesAndGraphs.cs: * TreesAndGraphsTest.cs:
    * src.csproj.nuget.g.props: Add 2 Tree and graph tasks

---
+++ src/TreesAndGraphs.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace src
+{
+    public enum State { Unvisited, Visited, Visiting }
+
+    public class Node
+    {
+        public string Name;
+        public Node[] Children = new Node[3];
+        public State State;
+    }
+
+
+    public class Graph
+    {
+        public Node[] Nodes = new Node[3];
+    }
+
+    public class TreeNode
+    {
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    public class TreesAndGraphs
+    { 
+        //Tasks
+
+        //4.1 Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+        public bool Task4and1(Graph g, Node start, Node end)
+        {
+            if (start==end)
+            {
+                return true;
+            }
+            var q = new LinkedList<Node>();
+            foreach (var n in g.Nodes)
+            {
+                n.State = State.Unvisited;
+            }
+            start.State = State.Visiting;
+            q.AddLast(start);
+            Node u;
+
+            while (q.Count > 0)
+            {
+                u = q.First.Value;
+                q.RemoveFirst();
+                if (u != null)
+                {
+                    foreach (var v in u.Children)
+                    {
+                        if (v != null)
+                        {
+                            if (v.State == State.Unvisited)
+                            {
+                                if (v == end)
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    v.State = State.Visiting;
+                                    q.AddLast(v);
+                                }
+                            }
+                        }
+                    }
+                    u.State = State.Visited;
+                }
+            }
+            return false;
+        }
+
+
+        //4.2 Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algo­
+        //rithm to create a binary search tree with minimal height.
+        public TreeNode CreateMinimalBST(int[] arr)
+        {
+            return CreateMinimalBST(arr, 0, arr.Length - 1);
+        }
+
+        public TreeNode CreateMinimalBST(int[] arr, int start, int end)
+        {
+            if (end < start)
+            {
+                return null;
+            }
+            int mid = (start + end) / 2;
+            TreeNode n = new TreeNode(arr[mid]);
+            n.left = CreateMinimalBST(arr, start, mid - 1);
+            n.right = CreateMinimalBST(arr, mid + 1, end);
+            return n;
+        }
+    }
+}
---
+++ test/TreesAndGraphsTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace src
+{
+    [TestClass]
+    public class TreesAndGraphsTest
+    {
+        [TestMethod]
+        public void Task4and1Test()
+        {
+            var cl = new TreesAndGraphs();
+
+            var g1 = new Graph();
+            var node1 = new Node() { Name = "1" };
+            var node2 = new Node() { Name = "2" };
+            var node3 = new Node() { Name = "3" };
+
+            g1.Nodes[0] = node1;
+            g1.Nodes[1] = node2;
+            g1.Nodes[2] = node3;
+
+            node1.Children[0] = node2;
+
+
+            Assert.IsFalse(cl.Task4and1(g1, node1, node3));
+
+
+            node2.Children[0] = node3;
+
+
+            Assert.IsTrue(cl.Task4and1(g1, node1, node3));
+        }
+
+        [TestMethod]
+        public void Task4and2Test()
+        {
+            var cl = new TreesAndGraphs();
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 12, 18 };
+            var res = cl.CreateMinimalBST(arr);
+            Assert.AreEqual(res.value, 4);
+         }
+    }
+}
